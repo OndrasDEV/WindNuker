e3ee5f3a50b2efde494014bc46a9308748240284
Ondras <101731277+OndrasDEV@users.noreply.github.com>
2023-06-14 14:14:29 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,14 +1,17 @@
-from colorama import Fore
 import os
 from spammer import spam
 from deleter import delete
+from rgbprint import gradient_print,Color
+
+def title():
+    os.system("title Wind Nuker V1 (made by ondras)")
 
 def clear():
     os.system("cls")
     
 def logo():
     clear()
-    print(Fore.CYAN,"""
+    gradient_print("""
     
 
 ██╗    ██╗██╗███╗   ██╗██████╗     ███╗   ██╗██╗   ██╗██╗  ██╗███████╗██████╗ 
@@ -20,16 +23,19 @@ def logo():
                                                                               
 
 
-    """)
+    """, start_color=Color.white,end_color=Color.blue)
 logo()
 
 
 def menu():
     while True:
         logo()
-        print("[1] Webhook Spammer")
-        print("[2] Webhook Deleter")
-        print("[0] Exit")
+        title()
+        gradient_print("""
+        [1] Webhook Spammer
+        [2] Webhook Deleter
+        [0] Exit
+        """,start_color=Color.blue,end_color=Color.dark_blue)
         choice = int(input(">"))
         if choice == 1:
             clear()
