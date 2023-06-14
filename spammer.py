a0ca5fb04cf0fab5576bea2b855d578ca9966838
Ondras <101731277+OndrasDEV@users.noreply.github.com>
2023-06-14 14:13:24 +0200

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,6 +1,8 @@
 from discord_webhook import DiscordWebhook
+import json
+config = json.load(open("config.json",encoding="utf-8"))
 def spam(looplen,webhooklink,msg):
     for i in range(looplen):
-        webhook = DiscordWebhook(url=webhooklink,rate_limit_retry=True, username="Wind Nuker V1",content=msg)
+        webhook = DiscordWebhook(url=webhooklink,rate_limit_retry=True, username=config["username"],content=msg)
         response = webhook.execute()
 
